060ca38cc3b1b8bba6df8b2ccf957b5d606c09ff
kyo <73341835+puxt@users.noreply.github.com>
2022-02-28 00:22:44 +0100

    - Changes
    changed '[@_!#$£%^&*()<>?/\|}{~:]' to string.punctuation

--- gen.py
+++ gen.py
@@ -1,20 +1,22 @@
+# Importing modules
 import random
 import string
 import sys
 
-#user input
+# User input
 lenght = int(input('Strings lenght : '))
 lines = int(input('How many lines to generate : '))
 
-def randStr (chars = '[@_!#$£%^&*()<>?/\|}{~:]' + string.ascii_letters + string.digits, N=lenght):
+def randStr (
+        chars = string.ascii_letters + string.digits + string.punctuation, N=lenght):
         return ''.join(random.choice(chars) for _ in range(N))
 
 print ("Generating output... press Ctrl+C to stop.")
 
-#output strings to file
+# Output strings to file
 file_path = 'output.txt'
 sys.stdout = open(file_path, "w")
 
-#generating strings
+# Generating strings
 for x in range(lines):
         print(randStr())
